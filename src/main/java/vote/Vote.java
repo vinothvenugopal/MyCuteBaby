b4b6e7e00deb4ec1d0b1ecc5e3375ed65bf1254e
Vinoth Venugopal <v.vinodhan@gmail.com>
2020-08-16 12:17:31 +0530

    initial commit

---
+++ src/main/java/vote/Vote.java
@@ -0,0 +1,51 @@
+package vote;
+
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Vote {
+
+	@Test
+	public void CastVote() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mycutebaby.in/contest/participant/?n=5f37f5ed82c7f");
+		WebElement ele = driver.findElementById("v");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);",ele);
+		List<WebElement> nameField = driver.findElementsById("v");
+		if(nameField.size()>0)
+		{
+			driver.findElementById("v").sendKeys("Priyadarshini");
+		}
+		else
+		{
+			System.out.println("NAme field not displayed");
+			System.exit(0);
+		}
+		driver.findElementById("vote_btn").click();
+		Thread.sleep(2000);
+		List<WebElement> popup = driver.findElementsByXPath("//button[text()='×']");
+		if(popup.size()>0)
+		{
+			js.executeScript("arguments[0].click()",popup.get(0));
+			System.out.println("pop up closed");
+		}
+		List<WebElement> voteMsg = driver.findElementsById("vote_msg");
+		if(voteMsg.size()>0)
+		{
+			System.out.println("Vote Successful");
+		}
+		else
+		{
+			System.out.println("Vote message not displayed. Vote unsuccessful probably");
+		}
+	}
+}
